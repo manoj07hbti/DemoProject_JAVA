14e582803720755d831756eb745d36fc4bb91e6f
60manojkumar@gmail.com <Motivated@2019>
2021-08-11 08:37:43 +0530

    Added Code for Java variables

---
+++ src/variables/VariableDemo.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class VariableDemo {
+
+    public static void main(String[] args) {
+
+        // SYNTAX
+        // DataType variable_name= value;
+
+        String name="Raj Kumar";
+        System.out.println(name);
+        String  cityName="AGRA";
+        System.out.println(cityName);
+
+        int age=25;
+        System.out.println(age);
+        int marks=76;
+        System.out.println(marks);
+
+        double salary=25765.3;
+        System.out.println(salary);
+        double price=232.2;
+        System.out.println(price);
+
+    }
+
+}

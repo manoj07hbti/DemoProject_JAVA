2de17335c9710d29e7ee29821ba330501320de50
60manojkumar@gmail.com <Motivated@2019>
2021-08-13 08:27:42 +0530

    Added Code for Loops Exercise

---
+++ src/loops_working/ExerciseLoop.java
@@ -0,0 +1,24 @@
+package loops_working;
+
+public class ExerciseLoop {
+
+    public static void main(String[] args) {
+
+        // SYNTAX :   for ( variable declaration ; condition ; increment/decrement ){ code }
+
+        for (int i=1; i<=10 ; i++ ){
+            int two=2;
+            int output= two*i;
+            System.out.println(output);
+        }
+
+        for (int i=1; i<=10 ; i++ ){
+            int five=5;
+            int output= five*i;
+            System.out.println(output);
+        }
+
+
+
+    }
+}

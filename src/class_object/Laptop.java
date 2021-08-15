f51ae6bf40dec3e26cda1c5746952887b7815986
60manojkumar@gmail.com <Motivated@2019>
2021-08-15 08:35:20 +0530

    Class and Object

---
+++ src/class_object/Laptop.java
@@ -0,0 +1,33 @@
+package class_object;
+
+public class Laptop {
+
+    //properties
+    String brand="Dell";// declaration
+    int ram=8;
+    double price=68000.5;
+    int hard_drive=512;
+
+    public static void main(String[] args) {
+        // SYNTAX for creating object
+        // CLASS_NAME  OBJ_NAME= new CLASS_NAME();
+        Laptop object = new Laptop();// Object1 created
+
+        System.out.println(object.brand); // object.property_name
+        System.out.println(object.ram);
+        System.out.println(object.hard_drive);
+        System.out.println(object.price);
+
+        Laptop object1 = new Laptop();// Object2 created
+
+        System.out.println(object1.brand); // object.property_name
+        System.out.println(object1.ram);
+        System.out.println(object1.hard_drive);
+        System.out.println(object1.price);
+
+
+    }
+
+
+
+}
